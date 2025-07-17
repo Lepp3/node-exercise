076c90cd5fab79a5d6e9e73caacce3642a53cfb7
Lepp3 <lubaka05@gmail.com>
2025-07-17 13:52:52 +0300

    added order controller and service

---
+++ src/entities/order/order.controller.ts
@@ -0,0 +1,58 @@
+import { Router, Request, Response } from 'express';
+import { OrderService } from './order.service.js';
+
+const orderController = Router();
+const orderService = new OrderService();
+
+orderController.get('/', async (req: Request, res: Response) => {
+  try {
+    const users = await orderService.getAll();
+    res.status(200).json(users);
+  } catch (error) {
+    res.status(500).json(error);
+  }
+});
+
+orderController.get('/:id', async (req: Request, res: Response) => {
+  const orderId = req.params.id;
+  try {
+    const order = await orderService.getById(orderId);
+    if (!order) {
+      return res.status(404).json({ message: 'Order not found' });
+    }
+    res.status(200).json(order);
+  } catch (error) {
+    res.status(500).json(error);
+  }
+});
+
+orderController.post('/', async (req: Request, res: Response) => {
+  const newOrderInfo = req.body;
+  try {
+    const newOrder = await orderService.create(newOrderInfo);
+    res.status(201).json(newOrder);
+  } catch (error) {
+    res.status(500).json(error);
+  }
+});
+
+orderController.put('/:id', async (req: Request, res: Response) => {
+  const orderId = req.params.id;
+  const updatedInfo = req.body;
+  try {
+    const updatedOrder = await orderService.update(orderId, updatedInfo);
+    res.status(200).json(updatedOrder);
+  } catch (error) {
+    res.status(500).json(error);
+  }
+});
+
+orderController.delete('/:id', async (req: Request, res: Response) => {
+  const orderId = req.params.id;
+  try {
+    await orderService.delete(orderId);
+    res.status(204).send();
+  } catch (error) {
+    res.status(500).json(error);
+  }
+});
---
+++ src/entities/order/order.service.ts
@@ -0,0 +1,46 @@
+import { OrderModel } from '../../models/index.js';
+import { OrderProperties } from './order.model.js';
+
+export class OrderService {
+  constructor(private readonly model = OrderModel) {}
+
+  async getAll(): Promise<OrderModel[]> {
+    return await this.model.findAll();
+  }
+
+  async getById(orderId: string): Promise<OrderModel | null> {
+    return await this.model.findByPk(orderId);
+  }
+
+  async create(orderData: OrderProperties): Promise<OrderModel> {
+    return await this.model.create(orderData);
+  }
+
+  async update(
+    orderId: string,
+    orderData: OrderProperties
+  ): Promise<OrderModel> {
+    const [count] = await this.model.update(orderData, {
+      where: { id: orderId },
+    });
+    if (count === 0) {
+      throw new Error(`Order with id ${orderId} not found`);
+    }
+    const updated = await this.model.findByPk(orderId);
+    if (!updated) {
+      throw new Error(`Order fetch failed after update`);
+    }
+    return updated;
+  }
+
+  async delete(orderId: string): Promise<void> {
+    const order = await this.model.findByPk(orderId, { paranoid: false });
+    if (!order) {
+      throw new Error(`Order with ID ${orderId} does not exist`);
+    }
+    if (order.deletedAt) {
+      throw new Error(`Order with ID ${orderId} is already deleted`);
+    }
+    await order.destroy();
+  }
+}
